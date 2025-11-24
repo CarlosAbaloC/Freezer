fe3436f43f55f3d2a51605d3f074a4e23b172425
JABALOC <jabalo1988@gmail.com>
2025-11-24 14:40:36 +0100

    Modal añadido

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
-window.onload = function() {
-this.alert("El script funciona")
-document.querySelector(".nav-carta").addEventListener("click", () => {
-    window.location.href = "carta.html";
-  });
-  document.querySelector(".nav-nosotros").addEventListener("click", () => {
-    window.location.href = "nosotros.html";
-  });
-  document.querySelector(".nav-encuentranos").addEventListener("click", () => {
-    window.location.href = "encuentranos.html";
-  });
-  document.querySelector(".nav-contacto").addEventListener("click", () => {
-    window.location.href = "contacto.html";
-  });
-}
+document.addEventListener("DOMContentLoaded", () => {
+
+  function configurarModal(botonSelector, modalId, cerrarId) {
+      const boton = document.querySelector(botonSelector);
+      const modal = document.getElementById(modalId);
+      const cerrar = document.getElementById(cerrarId);
+
+      boton.addEventListener("click", e => {
+        e.preventDefault();
+        modal.style.display = "flex";
+        document.body.classList.add("modal-open");
+      });
+
+      cerrar.addEventListener("click", () => {
+        modal.style.display = "none";
+        document.body.classList.remove("modal-open");
+      });
+
+      modal.addEventListener("click", (e) => {
+        if (e.target === modal) {
+          modal.style.display = "none";
+          document.body.classList.remove("modal-open");
+        }
+      });
+  }
+
+  
+
+  configurarModal(".btn-login", "modal-login", "cerrar-login");
+  configurarModal(".btn-register", "modal-register", "cerrar-registro");
+
+  
+
+
+});
